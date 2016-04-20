4c4af3238122d137b05fbc5da209586eca7b5374
Ahmed El-Kollaly <ahmed_elkollaly@hotmail.com>
2016-04-20 23:32:31 +0200

    TasteModel and TasteServices are modified

--- src/com/models/TasteModel.java
+++ src/com/models/TasteModel.java
@@ -1,5 +1,78 @@
 package com.models;
 
-public class TasteModel {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.mysql.jdbc.Statement;
+
+
+
+public class TasteModel {
+	String taste;
+	int id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTaste() {
+		return taste;
+	}
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+	public static boolean addTasteToUser(String taste,int id){
+		
+		TasteModel userTaste = addTaste(taste);
+		try {
+			Connection conn = DBConnection.getActiveConnection();
+			String sql = "Insert into usertaste (`user_id`,`taste_id`) VALUES  (?,?)";
+			// System.out.println(sql);
+
+			PreparedStatement stmt;
+			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, userTaste.id);
+			stmt.setInt(2, id);
+			
+			stmt.executeUpdate();
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				return true;
+			}
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	public static TasteModel addTaste(String taste){
+		try {
+			Connection conn = DBConnection.getActiveConnection();
+			String sql = "Insert into taste (`name`) VALUES  (?)";
+			// System.out.println(sql);
+
+			PreparedStatement stmt;
+			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, taste);
+			stmt.executeUpdate();
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				TasteModel userTaste = new TasteModel();
+				userTaste.id = rs.getInt(1);
+				userTaste.taste= taste;
+				return userTaste;
+			}
+			return null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
 }
--- src/com/services/TasteServices.java
+++ src/com/services/TasteServices.java
@@ -1,5 +1,37 @@
 package com.services;
 
-public class TasteServices {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.simple.JSONObject;
+
+import com.models.DBConnection;
+import com.models.TasteModel;
+import com.models.UserModel;
+import com.mysql.jdbc.Statement;
+
+@Path("/")
+public class TasteServices {
+	String taste;
+	
+	@POST
+	@Path("/addUserTaste")
+	@Produces(MediaType.TEXT_PLAIN)
+	public boolean addUserTaste(@FormParam("name") String taste,
+			@FormParam("id") int user_id) {
+		
+		boolean added = TasteModel.addTasteToUser(taste, user_id);
+	
+		return added;
+	}
+	
+	
 }
