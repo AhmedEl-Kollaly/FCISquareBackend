86e59de4ee56a52c699fa1a7f0e90cab4a30739c
AymanAliAli <aymanaliali@ymail.com>
2016-04-28 01:16:18 +0200

    Added files via upload

---
+++ src/com/models/Likes.java
@@ -0,0 +1,21 @@
+package com.models;
+
+public class Likes 
+{
+	private int likeID;
+	private int checkinID;
+	
+	public int getLikeID() {
+		return likeID;
+	}
+	public void setLikeID(int likeID) {
+		this.likeID = likeID;
+	}
+	public int getCheckinID() {
+		return checkinID;
+	}
+	public void setCheckinID(int checkinID) {
+		this.checkinID = checkinID;
+	}
+	
+}
---
+++ src/com/models/comments.java
@@ -0,0 +1,22 @@
+package com.models;
+
+public class comments 
+{
+	private int commentID;
+	private int checkinID;
+	
+	public int getCommentID() {
+		return commentID;
+	}
+	public void setCommentID(int commentID) {
+		this.commentID = commentID;
+	}
+	public int getCheckinID() {
+		return checkinID;
+	}
+	public void setCheckinID(int checkinID) {
+		this.checkinID = checkinID;
+	}
+	
+	
+}
